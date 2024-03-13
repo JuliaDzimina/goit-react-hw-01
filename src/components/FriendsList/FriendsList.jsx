b4f0ad5c2c67516f64b34eced96a974964194554
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2024-03-13 22:47:53 +0200

    task02

--- src/components/FriendsList/FriendsList.jsx
+++ src/components/FriendsList/FriendsList.jsx
@@ -1,8 +1,9 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
+import css from "./FriendList.module.css";
 
 const FriendsList = ({ friends }) => {
   return (
-    <ul>
+    <ul className={css.friendsList}>
       {friends.map(({ avatar, name, isOnline, id }) => {
         return (
           <li key={id}>
